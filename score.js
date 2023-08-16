fd8425ee7015232fbd0e69ff39f9f5bed0c7dedd
litaparth <119962833+litaparth@users.noreply.github.com>
2023-08-16 22:55:53 +0530

    Add files via upload

---
+++ score.js
@@ -0,0 +1,14 @@
+function newScreen(){
+    window.location="car_survey.html";
+}
+function fever(){
+    window.location="car_survey copy.html";
+}
+ function back(){
+    window.location="score.html";
+ }
+ function eyes(){
+    window.location="eye.html"; 
+ }
+ 
+ 
